1a9c5ab30ce134a8c0210c891b7ce96bf55dd331
Dima <dimapanteleyvrach@gmail.com>
2023-07-17 15:21:05 +0300

    коммит

---
+++ src/main/java/com/example/kursachnumber2/Kursachnumber2Application.java
@@ -0,0 +1,13 @@
+package com.example.kursachnumber2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursachnumber2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursachnumber2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kursachnumber2/Kursachnumber2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursachnumber2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursachnumber2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
